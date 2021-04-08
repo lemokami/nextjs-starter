23acdee294238f6464b4afe320ca5383582f9832
lemokami <jesswinw@gmail.com>
2021-04-08 11:21:15 +0530

    init 🚀

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.scss';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>lemokami's Nextjs Starter</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to lemokami's <a href='https://nextjs.org'>Next.js!</a>{' '}
+          Starter
+        </h1>
+      </main>
+    </div>
+  );
+}
