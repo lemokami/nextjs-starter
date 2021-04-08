04902e05ff643113e64339de49199d7a0d167a40
lemokami <jesswinw@gmail.com>
2021-04-08 12:12:55 +0530

    made this starter worth calling custom starter😌

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>lemokami's Nextjs Starter</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/favicon.png' />
       </Head>
 
       <main className={styles.main}>
